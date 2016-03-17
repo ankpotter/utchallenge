4bd2881bae7eaf8695781471abf5f7992b104395
ankpotter <ankitharrypotter@gmail.com>
2016-03-17 10:55:20 +0530

    Added files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http')
+   , fs = require('fs')
+   ;
+
+var server = http.createServer(function (req, res) {
+  fs.readFile(__dirname + '/message.txt', function (err, data) {
+    res.end(data);
+  });
+});
+
+server.listen(80);
